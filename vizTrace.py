be73089de112f27b3a0dbf10b0a8f6a735b2ed81
nlee113 <82241838+nlee113@users.noreply.github.com>
2024-04-05 15:56:16 -0700

    intermediate hops are displayed

--- vizTrace.py
+++ vizTrace.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import subprocess
+import re
 
 app = Flask(__name__)
 
@@ -9,11 +10,12 @@ def index():
 
 @app.route('/trace', methods=['POST'])
 def trace():
-    hostname = request.form['hostname']  # Extract hostname 
+    hostname = request.form['hostname']
     print("Received hostname:", hostname)
     output = run_traceroute(hostname)
     print(output)  # Print traceroute output in terminal
-    return {'output': output}
+    ip_addresses = parse_traceroute_output(output)
+    return jsonify({'ip_addresses': ip_addresses})
 
 def run_traceroute(hostname):
     try:
@@ -23,6 +25,11 @@ def run_traceroute(hostname):
     except subprocess.CalledProcessError as e:
         # Handle traceroute command error
         return f"Error: {e}"
+
+def parse_traceroute_output(output):
+    # Extract IP addresses from the output
+    ip_addresses = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', output)
+    return ip_addresses
 
 if __name__ == '__main__':
     app.run(debug=True)
